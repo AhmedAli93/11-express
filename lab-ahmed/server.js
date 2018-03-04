be9cc450a403ff69d711ea16d1c124d426f30c62
Ahmed Ali <Ahmed98148@gmail.com>
2018-03-04 14:40:07 -0800

    fixing errors

--- lab-ahmed/server.js
+++ lab-ahmed/server.js
@@ -1,48 +1,46 @@
-import { create } from 'domain';
-
-'use strict'
+'use strict';
 
 const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
 const jsonParser = require('body-parser').json();
-const debug = require('debug')('note:server');
+const debug = require('debug')('car:server');
+const Car = require('./model/car.js');
 
-const Note = require('./model/note.js');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
-
 
 app.use(morgan('dev'));
 
-app.get('/test', function(req, res){
+app.get('/test', function(req, res) {
   debug('GET: /test');
-  res.json({msg: 'hello from /test'});
+  res.json({ msg: 'hello from /test' });
 });
 
-app.post('/api/note', jsonParser, function(res, req, next){
-  debug('POST: /api/note');
-  Note.createNote(req.body);
-  .then(note => res.json(note))
-  .catch(err =>) next(err));
-}):
-
-app.get('/api/note', function(req, res, next){
-  debug('GET: /api/note');
-
-  Note.fetchNote(req.query.id)
-  .then( note => res.json(note))
-  .catch(err => next(err))
+app.post('/api/car', jsonParser, function(req, res, next) {
+  debug('POST: /api/car');
+  Car.createCar(req.body)
+    .then( car => res.json(car))
+    .catch( err => next(err));
 });
 
-app.use(function(err,req,res,next){
-  debug('err middleware');
-  console.err(err.message);
+app.get('/api/car/:carId', function(req, res, next) {
+  debug('GET: /api/car/:carId');
 
-  if(err.status){
+  Car.fetchCar(req.params.carId)
+    .then( car => res.json(car))
+    .catch( err => next(err));
+});
+
+app.use(function(err, req, res) {
+  debug('error middleware');
+  console.error(err.message);
+
+  if (err.status) {
     res.status(err.status).send(err.name);
-    return
+    return;
   }
+
   err = createError(500, err.message);
   res.status(err.status).send(err.name);
 });
